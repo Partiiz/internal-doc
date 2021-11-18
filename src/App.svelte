e4b5f278dee83633656b346d3cc7eba896b879f0
Maxime Di Natale <maxime@oddiris.com>
2021-11-18 14:09:48 +0100

    Initialization

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<main>
+  <h1>DOC</h1>
+</main>
+
+<style global>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    color: #333;
+  }
+
+  *,
+  *::after,
+  *::before {
+    padding: 0;
+    margin: 0;
+
+    box-sizing: border-box;
+
+    transition: none 600ms cubic-bezier(0, 0.5, 0, 1);
+  }
+</style>
