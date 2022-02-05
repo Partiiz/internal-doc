d49f60bca887148427130336c397b016bc5646c4
PivotStick <pivoorigami@gmail.com>
2022-02-05 12:02:26 +0100

    welcome page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,3 @@
-<h1>Partiiz Internal Doc</h1>
+<h1>Bienvenue sur le cahier des charges de Partiiz !</h1>
+<p>Cette <em>github page</em> est fréquemment mis à jour.</p>
+<p>Il est donc très fiable d'y aller pour se documenter.</p>
