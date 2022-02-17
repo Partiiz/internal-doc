c655cbb3944d4498d2d52f5bae467eba17e97748
rgonzalez <rgonzalez@smart4.io>
2022-02-17 10:32:19 +0100

    ajouts carte expirée

---
+++ src/routes/Soirées/Carte Expirée.svelte
@@ -0,0 +1,10 @@
+<p>-> Nom de la soirée</p>
+<p>-> Photo du lieu (+ photos pendant la soirée pour montrer comment elle s'est passé, montrer les amitiés qui se sont formés etc si l'utilisateur décide d'en ajouter)</p>
+<p>-> Localisation de la soirée</p>
+<p>-> Date de la soirée + "(Terminée)"</p>
+<p>-> Description de la soirée</p>
+<p>-> Filtres de la soirée</p>
+<p>-> Nombre de personnes qui ont été présente à la soirée (l'utilisateur peut entrer cette information après la soirée si il le souhaite. sinon, le nombre de personne qui a été défini de base est affiché) (Les membres ne sont pas dévoilés)</p>
+<p>-> Commentaires et notes des utilisateurs qui ont été présents à la soirée.</p>
+<p>-> Bouton pour signaler la soirée (Voir la partie "CRITERES DE SIGNALEMENT")</p>
+<p>-> Bouton pour signaler l'organisateur de la soirée (bouton visible seulement si c'est une soirée qui a été demandé et que l'utilisateur qui regarde la carte est le demandeur)</p>
