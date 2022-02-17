3027e81041dec2fe69d774cee04f236acbc0cb27
PivotStick <pivoorigami@gmail.com>
2022-02-17 10:12:30 +0100

    Netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const PROD = process.env.NODE_ENV === 'production';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		paths: {
-			base: PROD ? '/partiiz_internal_doc' : ''
-		},
 		adapter: adapter(),
 		target: 'body'
 	}
